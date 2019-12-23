2e8445561b6d16334ae4e75315fcc287838d44ee
Hannah Bishop <hannahbananabishop@gmail.com>
2019-12-23 11:56:19 -0800

    Use DecimalSequence aggregator in translator
    Now that a decimal sequence can aggregate itself, the translator
    can turn any individual roman numeral sequence into an integer
    without operating directly on the sequence.
    
    Again, this does not handle any validation of the input, that should
    be done elsewhere.

--- spec/roman_decimal_translator_spec.rb
+++ spec/roman_decimal_translator_spec.rb
@@ -4,51 +4,65 @@ describe RomanDecimalTranslator do
   translator = RomanDecimalTranslator.new()
   
   it "translates i to 1" do
-    test_string = "ii"
+    test_string = "I"
     translation = translator.call(test_string)
 
-    expect(translation.value).to eq([1, 1])
+    expect(translation).to eq(1)
   end
 
   it "translates v to 5" do
-    test_string = "vi"
+    test_string = "V"
     translation = translator.call(test_string)
 
-    expect(translation.value).to eq([5, 1])
+    expect(translation).to eq(5)
   end
 
   it "translates x to 10" do
-    test_string = "xxv"
+    test_string = "x"
     translation = translator.call(test_string)
 
-    expect(translation.value).to eq([10, 10, 5])
+    expect(translation).to eq(10)
   end
 
   it "translates l to 50" do
-    test_string = "lxxi"
+    test_string = "L"
     translation = translator.call(test_string)
 
-    expect(translation.value).to eq([50, 10, 10, 1])
+    expect(translation).to eq(50)
   end
 
   it "translates c to 100" do
-    test_string = "ccc"
+    test_string = "C"
     translation = translator.call(test_string)
 
-    expect(translation.value).to eq([100, 100, 100])
+    expect(translation).to eq(100)
   end
 
   it "translates d to 500" do
-    test_string = "dccxi"
+    test_string = "D"
     translation = translator.call(test_string)
 
-    expect(translation.value).to eq([500, 100, 100, 10, 1])
+    expect(translation).to eq(500)
   end
 
   it "translates m to 1000" do
-    test_string = "mmdccv"
+    test_string = "m"
     translation = translator.call(test_string)
 
-    expect(translation.value).to eq([1000, 1000, 500, 100, 100, 5])
+    expect(translation).to eq(1000)
+  end
+
+  it "translates multi-character numerals" do
+    test_string = "mdcclxxvii"
+    translation = translator.call(test_string)
+
+    expect(translation).to eq(1777)
+  end
+
+  it "translates multi-character numerals with subtraction terms" do
+    test_string = "MCDXCIV"
+    translation = translator.call(test_string)
+
+    expect(translation).to eq(1494)
   end
 end
